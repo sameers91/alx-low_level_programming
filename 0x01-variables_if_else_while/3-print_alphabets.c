eb1eaad5d13351e108f58cdd4fba2cd1346548df
sameers91 <sammoosa.sm@gmail.com>
2022-09-09 22:32:45 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,20 +8,20 @@
 */
 int main(void)
 {
-  char ch = 'a';
-  char CH = 'A';
-  /*prints a - z*/
-  while (ch <= 'z')
-    {
-      putchar(ch);
-      ++ch;
-    }
-  /*print A - Z*/
-  while (CH <= 'Z')
-    {
-      putchar(CH);
-      ++CH;
-    }
-  putchar('\n');
-  return (0);
+char ch = 'a';
+char CH = 'A';
+/*prints a - z*/
+while (ch <= 'z')
+{
+putchar(ch);
+++ch;
+}
+/*print A - Z*/
+while (CH <= 'Z')
+{
+putchar(CH);
+++CH;
+}
+putchar('\n');
+return (0);
 }
