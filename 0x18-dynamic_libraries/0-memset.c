ad820b81e34e59c17635303111194d941909402b
sameers91 <sammoosa.sm@gmail.com>
2022-12-15 21:05:17 +0300

    Create 0-memset.c

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,14 @@
+/**
+* _memset -  description
+*
+* @s: input function argument
+* @b: input function argument
+* @n: input function argument
+*
+* Return: output
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+return (0);
+}
